74c955dc8070c3de8e7586c765180e639f8dfb09
Ay Nio <agidummy2@gmail.com>
2024-01-23 15:59:49 -0600

    Basic Enemy AI and Navigation
    Added Basic Enemies that:
    - patrol when no player in sight range
    - chase when player is in sight range
    - attack when player is in attack range
    
    Colors and Gizmos for visual clarity
    Added and baked Navmesh into a New Scene

---
+++ CS377IsoGame/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask Walkable, whatIsPlayer;
+
+    private Renderer enemyRenderer;
+
+    // Patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    // Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    // States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        enemyRenderer = GetComponent<Renderer>();
+
+    }
+
+    private void Update()
+    {
+        // Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+
+
+
+    }
+
+    private void Patroling()
+    {
+        ChangeColor(Color.blue);
+
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        // Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    private void SearchWalkPoint()
+    {
+        // Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, Walkable))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        Debug.Log("Patroling");
+        ChangeColor(Color.yellow);
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        Debug.Log("Attacking");
+
+        ChangeColor(Color.red);
+        // For now enemy stops when in attack range
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            // attack code here
+
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    // Method to change the color of the enemy
+    private void ChangeColor(Color color)
+    {
+        if (enemyRenderer != null)
+        {
+            enemyRenderer.material.color = color;
+        }
+    }
+
+    // Draw attack and sight range with gizmos
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+
+    }
+}
