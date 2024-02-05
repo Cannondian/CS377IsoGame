d4097230b97606c6081186247bd879722e251a2d
Ay Nio <agidummy2@gmail.com>
2024-02-05 16:04:11 -0600

    Enemies now take damage and die
    simple script WeaponTrigger on the Weapon to detect enemy triggers

--- CS377IsoGame/Assets/Scripts/PlayerCombatCollisions.cs
+++ CS377IsoGame/Assets/Scripts/PlayerCombatCollisions.cs
@@ -16,46 +16,50 @@ namespace EgeCode
         void Start()
         {
             controller = GetComponent<RPGCharacterController>();
+            Debug.Log("Started!");
         }
 
-        private void OnCollisionEnter(Collision collision)
+        private void OnTriggerEnter(Collider other)
         {
-            Collider myCollider = collision.GetContact(0).thisCollider;
+            // This assumes that the script is attached to the object with the weaponCollider and legCollider
+            Collider myCollider = GetComponent<Collider>();
             int attackNumber = controller.comboIndex;
 
-            if (controller.isAttacking && myCollider == weaponCollider && attackNumber == 3)
-            {
-                EventBus.TriggerEvent(EventTypes.Events.ON_BASIC_ATTACK_HIT,
-                    new EventTypes.Event9Param(collision.contacts[0].point, FXList.FXlist.ElectricHit, myCollider.transform.rotation));
-
-                EventBus.TriggerEvent(EventTypes.Events.ON_ENERGY_GAIN,
-                    CharacterEnergy.Instance.energyFromEnhancedBasic);
-
-                EventBus.TriggerEvent(EventTypes.Events.ON_JISA_ENHANCED_ATTACK, true);
-
-                DamageEnemy(collision, 10f);
-            }
-
-            else if (controller.isAttacking && (myCollider == weaponCollider || (myCollider == legCollider && attackNumber == 5)))
-            {
-                EventBus.TriggerEvent(EventTypes.Events.ON_LIFE_CURRENT_GAIN,
-                    new EventTypes.Event5Param(attackNumber, 0));
-
-                EventBus.TriggerEvent(EventTypes.Events.ON_BASIC_ATTACK_HIT,
-                    new EventTypes.Event9Param(collision.contacts[0].point, FXList.FXlist.BasicHit2, myCollider.transform.rotation));
-                
-                DamageEnemy(collision, 10f);
-            }
+            Debug.Log("Enemy hit:" + other.name);
         }
+        //    if (controller.isAttacking && myCollider == weaponCollider && attackNumber == 3)
+        //    {
+        //        EventBus.TriggerEvent(EventTypes.Events.ON_BASIC_ATTACK_HIT,
+        //            new EventTypes.Event9Param(other.transform.position, FXList.FXlist.ElectricHit, myCollider.transform.rotation));
 
-        // TODO: Update this! We could use the event bus, for instance (and/or some other more general function)
-        void DamageEnemy(Collision collision, float damage)
-        {
-            var enemyScript = collision.gameObject.GetComponent<EnemyAI>();
-            if (enemyScript != null) 
-            {
-                enemyScript.TakeDamage(damage);
-            }
-        }
+        //        EventBus.TriggerEvent(EventTypes.Events.ON_ENERGY_GAIN,
+        //            CharacterEnergy.Instance.energyFromEnhancedBasic);
+
+        //        EventBus.TriggerEvent(EventTypes.Events.ON_JISA_ENHANCED_ATTACK, true);
+
+        //        DamageEnemy(other, 10f);
+        //    }
+
+        //    else if (controller.isAttacking && (myCollider == weaponCollider || (myCollider == legCollider && attackNumber == 5)))
+        //    {
+        //        EventBus.TriggerEvent(EventTypes.Events.ON_LIFE_CURRENT_GAIN,
+        //            new EventTypes.Event5Param(attackNumber, 0));
+
+        //        EventBus.TriggerEvent(EventTypes.Events.ON_BASIC_ATTACK_HIT,
+        //            new EventTypes.Event9Param(other.transform.position, FXList.FXlist.BasicHit2, myCollider.transform.rotation));
+
+        //        DamageEnemy(other, 10f);
+        //    }
+        //}
+
+        //// Updated to use Collider parameter
+        //void DamageEnemy(Collider other, float damage)
+        //{
+        //    var enemyScript = other.GetComponent<EnemyAI>();
+        //    if (enemyScript != null)
+        //    {
+        //        enemyScript.TakeDamage(damage);
+        //    }
+        //}
     }
 }
