de62bd8c4f594126fa75fe742deb828189419b43
Mahebub Aalam Khatri <makhatri2023@u.northwestern.edu>
2024-02-10 00:26:48 -0600

    add target indicator to grenadier and more logic

--- CS377IsoGame/Assets/Scripts/Enemy/JumpingGrenadierController.cs
+++ CS377IsoGame/Assets/Scripts/Enemy/JumpingGrenadierController.cs
@@ -9,24 +9,31 @@ public class JumpingGrenadierController : EnemyAI
 
     [SerializeField] GameObject GrenadePrefab;
     [SerializeField] GameObject MuzzleFX;
+    [SerializeField] Transform Indicator;
 
-    public float TurnRate; // How quickly the head rotates to track the player
+    public float IndicatorSpeed; // How quickly the target indicator can follow the player
     public int BurstCount; // How many grenades are shot in quick succession for each time the enemy fires
     public float BurstInterval; // Time between grenades within a single burst
     public float CooldownTime; // Time between burst shot fires
     public float VerticalLaunchVelocity;
+    public float MaxFireDistanceToPlayer; // Maximum allowable distance between indicator and player position to enable firing
     
     public bool EnableLaunchVariation;
-    public float LaunchVariationMaximum; // Max amount of launch variation as a function of distance to player
+    public float LaunchVariationMaximum; // Max amount of launch velocity variation as a function of distance to player
 
     private Vector3 LaunchOffset = new Vector3(0f, 1.1f, 0f); // Offset from turret origin where grenade is fired
     private float LastBurstFireTime;
     private bool Attacking; // Indicates if currently in firing sequence
     private Vector3 ToPlayer;
 
+    private bool IsIndicatorOn;
+
     protected override void Awake()
     {
         base.Awake();
+
+        Indicator.gameObject.SetActive(false);
+        ResetIndicator();
 
         // prevent this enemy from attacking right away
         LastBurstFireTime = Time.time;
@@ -35,27 +42,44 @@ public class JumpingGrenadierController : EnemyAI
         AttackDamage = 10f;
     }
 
+    void Update()
+    {
+        // We put aiming here to enable smoother tracking for the indicator
+        if (IsIndicatorOn && !Attacking) 
+        {
+            AimTurret();
+        }
+    }
+
     void AimTurret()
     {
         ToPlayer = player.position - TurretTransform.position; // vector from turret to player
+        Indicator.position = new Vector3(
+            Mathf.Lerp(Indicator.position.x, player.position.x, IndicatorSpeed * Time.deltaTime),
+            player.position.y,
+            Mathf.Lerp(Indicator.position.z, player.position.z, IndicatorSpeed * Time.deltaTime)
+        );
 
-        // Determine the left/right angle to the player
-        float turretAngle = Vector3.SignedAngle(Vector3.forward, ToPlayer, Vector3.up);
+        // Scale indicator size with distance to show decrease in accuracy
+        float HorizontalDistanceToPlayer = new Vector2(Indicator.position.x, Indicator.position.z).magnitude;
+        float IndicatorScale = Mathf.Clamp(40f * LaunchVariationMaximum * HorizontalDistanceToPlayer / sightRange, 0.5f, 4f);
+        Indicator.localScale = new Vector3(IndicatorScale, IndicatorScale, IndicatorScale);
 
-        // Smooth rotation from current angle to desired angle
-        // The -60f is to keep the green indicator facing the player
-        TurretTransform.rotation = Quaternion.Slerp(TurretTransform.rotation, Quaternion.Euler(0f, turretAngle - 60f, 0f), TurnRate * Time.deltaTime);
+        // Determine the left/right angle to the indicator
+        float turretAngle = Vector3.SignedAngle(Vector3.forward, Indicator.position, Vector3.up);
 
-        // If we want the bottom section to spin opposite the "head"
+        // The -60f is to keep the green bar facing the indicator
+        TurretTransform.rotation = Quaternion.Euler(0f, turretAngle - 60f, 0f);
+
+        // Make the bottom section to spin opposite the "head" (if we want stationary, set it to Quaternion.identity)
         TurretBottomTransform.rotation = Quaternion.Inverse(TurretTransform.rotation);
-        
-        // If we want the bottom section to remain stationary relative to the body
-        // TurretBottomTransform.rotation = Quaternion.identity;
     }
 
     void MaybeFire()
     {
-        if (Time.time - LastBurstFireTime > CooldownTime && !Attacking)
+        if (Time.time - LastBurstFireTime > CooldownTime
+            && !Attacking 
+            && (player.position - Indicator.position).magnitude < MaxFireDistanceToPlayer)
         {
             Attacking = true;
             StartCoroutine(BurstFire());
@@ -64,10 +88,8 @@ public class JumpingGrenadierController : EnemyAI
 
     IEnumerator BurstFire() 
     {
-        var FireInstanceToPlayer = player.position - TurretTransform.position;
-
-        int i = 0;
-        while (i < BurstCount)
+        float TimeToGround = 0f;
+        for (int i = 0; i < BurstCount; i++)
         {
             // Firing effect at end of barrel
             Destroy(
@@ -88,10 +110,11 @@ public class JumpingGrenadierController : EnemyAI
 
             // Determine launch velocity
             float TimeToApex = VerticalLaunchVelocity / -Physics.gravity.y;
-            float TimeToGround = TimeToApex + Mathf.Sqrt(2f * (Grenade.transform.position.y + VerticalLaunchVelocity * TimeToApex) / -Physics.gravity.y);
-            float HorizontalDistanceToPlayer = new Vector2(FireInstanceToPlayer.x, FireInstanceToPlayer.z).magnitude;
+            TimeToGround = TimeToApex + Mathf.Sqrt(2f * (Grenade.transform.position.y + VerticalLaunchVelocity * TimeToApex) / -Physics.gravity.y);
+            float HorizontalDistanceToPlayer = new Vector2(Indicator.position.x, Indicator.position.z).magnitude;
             float HorizontalLaunchVelocity = (HorizontalDistanceToPlayer / TimeToGround) + HorizontalDistanceToPlayer * 0.05f; // last part is a fudge factor to make sure targeting is perfect
 
+            // Potentially add variation in horizontal launch velocity
             float LaunchVariationX = 0f;
             float LaunchVariationZ = 0f;
             if (EnableLaunchVariation)
@@ -101,32 +124,65 @@ public class JumpingGrenadierController : EnemyAI
             }
 
             GrenadeRB.velocity = new Vector3(
-                HorizontalLaunchVelocity * FireInstanceToPlayer.x / HorizontalDistanceToPlayer + LaunchVariationX,
+                HorizontalLaunchVelocity * Indicator.position.x / HorizontalDistanceToPlayer + LaunchVariationX,
                 VerticalLaunchVelocity,
-                HorizontalLaunchVelocity * FireInstanceToPlayer.z / HorizontalDistanceToPlayer + LaunchVariationZ
+                HorizontalLaunchVelocity * Indicator.position.z / HorizontalDistanceToPlayer + LaunchVariationZ
             );
 
             var settings = Grenade.GetComponent<GrenadierMissile>();
             settings.Damage = AttackDamage;
             // Todo: any more settings?
 
-            i += 1;
+            DisableIndicator();
             yield return new WaitForSeconds(BurstInterval);
+            EnableIndicator();
         }
+
+        // wait until last grenade hits ground before aiming again
+        yield return new WaitForSeconds(TimeToGround);
 
         Attacking = false;
         LastBurstFireTime = Time.time;
     }
 
+    protected override void Patroling()
+    {
+        ResetIndicator();
+        DisableIndicator();
+    }
+
     protected override void ChasePlayer()
     {
-        AimTurret();
+        EnableIndicator();
     }
 
     protected override void AttackPlayer()
     {
-        AimTurret();
+        EnableIndicator();
         MaybeFire();
+    }
+
+    void ResetIndicator()
+    {
+        Indicator.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+    }
+
+    void EnableIndicator()
+    {
+        if (!IsIndicatorOn)
+        {            
+            Indicator.gameObject.SetActive(true);
+            IsIndicatorOn = true;
+        }
+    }
+
+    void DisableIndicator()
+    {
+        if (IsIndicatorOn)
+        {
+            Indicator.gameObject.SetActive(false);
+            IsIndicatorOn = false;
+        }
     }
 
     protected override void UpdateCanvas()
