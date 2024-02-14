383ba568113fa44338d078fdf0b00a495ed87847
Cannondian <herrerame3@gmail.com>
2024-02-13 23:21:18 -0600

    Added New Level Layouts for Enemy Pairing Testing

---
+++ CS377IsoGame/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    enum EnemyType { Tank, Turret, Drone, Melee }
+    [SerializeField] private EnemyType enemyType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
